7d9d73058de6e6bb22f601aa2dc7be6773ee910f
santakurow <newtakuproj0730@gmail.com>
2020-05-11 21:59:25 +0900

    created action_text form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.with_rich_text_body_and_embeds
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "投稿しました。"
+      redirect_to root_path
+    else
+      flash.now[:danger] = "投稿に失敗しました。"
+      render :new
+    end
   end
 
   def show
@@ -18,5 +28,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
